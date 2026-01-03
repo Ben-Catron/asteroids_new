b278593bc82f926c4dae2afa2a5bf5014743b452
Ben-Catron <bencatron@hotmail.com>
2026-01-02 22:35:48 -0500

    Gameloop

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pygame
+from constants import *
+from logger import log_state
+
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+    
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+        dt = Clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
